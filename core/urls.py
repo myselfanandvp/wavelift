814535677a55865b8f093e96a199896ceb0d3a41
myselfanandvp <mailanandvp@gmail.com>
2025-07-19 06:53:20 +0530

    added the terms page and the contactus page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePage,Contactus,Aboutus
+from .views import HomePage,Contactus,Aboutus,Term_And_Condition,Policy
 
 urlpatterns =[
     path('',HomePage.as_view(),name="index_page"),
     path('contact/',Contactus.as_view(),name="contact_page"),
     path('about/',Aboutus.as_view(),name="about_page"),
+    path('terms/',Term_And_Condition.as_view(),name="term_and_conditon_url"),
+    path('policy',Policy.as_view(),name='policy_url')
 ]
--- core/views.py
+++ core/views.py
@@ -17,3 +17,14 @@ class Aboutus(View):
     template_name='core/about.html'
     def get(self,request):
         return render(request,self.template_name,{})
+    
+    
+class Term_And_Condition(View):
+    template_name="core/terms-and-condition.html"
+    def get (self,request):
+        return render(request,self.template_name,{})
+    
+class Policy(View):
+    template_name = "core/privacy_policy.html"
+    def get (self,request):
+        return render(request,self.template_name,{})
